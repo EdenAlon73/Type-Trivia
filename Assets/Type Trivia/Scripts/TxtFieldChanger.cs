36732ce6921a08b505d8f85901463257215bcafd
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-04-07 15:44:23 +0300

    cleaned keyboard script

--- Assets/Type Trivia/Scripts/TxtFieldChanger.cs
+++ Assets/Type Trivia/Scripts/TxtFieldChanger.cs
@@ -6,34 +6,18 @@ using UnityEngine.UI;
 
 public class TxtFieldChanger : MonoBehaviour
 {
-    [SerializeField]private KeyboardScript keyboardScript;
-    [SerializeField] private InputField inputField;
-    [SerializeField] private InputField inputField2;
-    [SerializeField] private InputField inputField3;
+    
+    private TouchScreenKeyboard _touchScreenKeyboard;
+   
     [SerializeField] private InputField[] inputFields;
-    [SerializeField] private Image first;
-    [SerializeField] private Image second;
-    [SerializeField] private Image third;
+    [SerializeField] private string[] lattersToGuess;
     [SerializeField] private Color red;
     [SerializeField] private Color green;
-    [SerializeField] private string latterToGuess = "s";
-    [SerializeField] private string latterToGuess2 = "a";
-    [SerializeField] private string latterToGuess3 = "y";
-    [SerializeField] private string[] lattersToGuess;
+    [SerializeField] private int fieldIndex = 0;
     private bool gotLetterRight = false;
     private bool startedWriting = false;
-    private bool gotSecondWord = false;
-    private bool gotThirdWord = false;
-    private bool started = false;
-    private bool started2 = false;
-    private bool started3 = false;
-    private bool onSecondField = false;
-    private bool onThirdField = false;
-    [SerializeField] private int fieldIndex = 0;
-    private void Awake()
-    {
-       
-    }
+    
+    
     private void Start()
     {
         SelectFirstField();
@@ -41,9 +25,8 @@ public class TxtFieldChanger : MonoBehaviour
     private void Update()
     {
         GotFieldRight();
-       // ChangeColor();
-        
     }
+    
     private void GotFieldRight()
     {
         if (inputFields[fieldIndex].text.Equals(lattersToGuess[fieldIndex]))
@@ -60,64 +43,12 @@ public class TxtFieldChanger : MonoBehaviour
                 ChangeColor();
             }
         }
-        /*
-        if (inputField.text.Equals(latterToGuess))
-        {
-            if (!gotFirstWord)
-            {
-                gotFirstWord = true;
-                ChangeField();
-            }
-        }
-      
-        if (inputField.text.Length > 0)
-        {
-            started = true;
-        }
-
-        if (inputField2.text.Equals(latterToGuess2))
-        {
-            gotSecondWord = true;
-            ChangeField();
-        }
-        if(inputField2.text.Length > 0)
-        {
-            started2 = true;
-        }
-
-        if (inputField3.text.Equals(latterToGuess3))
-        {
-            gotThirdWord = true;
-        }
-        if(inputField3.text.Length > 0)
-        {
-            started3 = true;
-        }
-        */
     }
 
     private void ChangeField()
     {
         fieldIndex++;
         SelectNextField();
-      //  var currentInputFieldIndex = inputFields[].ac
-        /*
-        if (keyboardScript.TextField == inputField)
-        {
-            keyboardScript.TextField = inputField2;
-            onSecondField = true;
-            print("Changing Field");
-        }
-        if (gotSecondWord)
-        {
-            if (keyboardScript.TextField == inputField2)
-            {
-                keyboardScript.TextField = inputField3;
-                onThirdField = true;
-                print("ya Changing Field");
-            }
-        }
-        */
     }
     private void ChangeColor()
     {
@@ -136,57 +67,7 @@ public class TxtFieldChanger : MonoBehaviour
                 startedWriting = false;
             }
         }
-
-        /*
-        if (started)
-        {
-            
-                if (!gotLetterRight)
-                {
-                    first.color = red;
-                    inputField.Select();
-                    inputField.text = "";
-                }
-                else
-                {
-                    first.color = green;
-                }
-
-            if (onSecondField)
-            {
-                if (started2)
-                {
-
-                    if (!gotSecondWord)
-                    {
-                        second.color = red;
-                        inputField2.Select();
-                        inputField2.text = "";
-                    }
-                    else
-                    {
-                        second.color = green;
-                    }
-                }
-            }
-        }
-        if (onThirdField)
-        {
-            if (started3)
-            {
-                if (!gotThirdWord)
-                {
-                    third.color = red;
-                    inputField3.Select();
-                    inputField3.text = "";
-                }
-                else
-                {
-                    third.color = green;
-                }
-            }
-        }
-        */
+        
        
     }
 
@@ -199,7 +80,6 @@ public class TxtFieldChanger : MonoBehaviour
     {
         if(fieldIndex < inputFields.Length)
         {
-            //inputFields[fieldIndex].ActivateInputField();
             inputFields[fieldIndex].Select();
         }
     }
